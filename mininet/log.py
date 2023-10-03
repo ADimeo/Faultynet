44ec51ec3bd86dc10cfea88448048cb313315851
Antonio Dimeo <antonio.dimeo@student.hpi.de>
2023-10-04 01:41:02 +1200

    Refactor log messages to include stack traces

--- mininet/log.py
+++ mininet/log.py
@@ -22,8 +22,9 @@ LEVELS = { 'debug': logging.DEBUG,
 # change this to logging.INFO to get printouts when running unit tests
 LOGLEVELDEFAULT = OUTPUT
 
+
 # default: '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
-LOGMSGFORMAT = '%(message)s'
+LOGMSGFORMAT = '%(filename)s %(lineno)d  %(message)s'
 
 
 # Modified from python2.5/__init__.py
@@ -147,12 +148,15 @@ def makeListCompatible( fn ):
     """Return a new function allowing fn( 'a 1 b' ) to be called as
        newfn( 'a', 1, 'b' )"""
 
-    def newfn( *args ):
+    def newfn( *args,  **kwargs):
         "Generated function. Closure-ish."
+        # Add stacklevel argument, or all linenos will point to
+        # this function
+        kwargs['stacklevel'] = kwargs.get('stacklevel', 1) + 1
         if len( args ) == 1:
-            return fn( *args )
+            return fn( *args, **kwargs )
         args = ' '.join( str( arg ) for arg in args )
-        return fn( args )
+        return fn( args, **kwargs )
 
     # Fix newfn's name and docstring
     setattr( newfn, '__name__', fn.__name__ )
