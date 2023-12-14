24ab68d9cac09b6d523085f5bf30bf0d43e1a424
Antonio Dimeo <antonio.dimeo@student.hpi.de>
2023-12-14 14:06:39 +0100

    Fix Faultynet -> faultynet further

--- util/install.sh
+++ util/install.sh
@@ -199,7 +199,7 @@ function mn_deps {
     fi
 
     echo "Installing Faultynet core"
-    pushd $MININET_DIR/Faultynet
+    pushd $MININET_DIR/faultynet
     sudo PYTHON=${PYTHON} make install
     popd
 }
@@ -235,7 +235,7 @@ function of {
     cd $BUILD_DIR/openflow
 
     # Patch controller to handle more than 16 switches
-    patch -p1 < $MININET_DIR/Faultynet/util/openflow-patches/controller.patch
+    patch -p1 < $MININET_DIR/faultynet/util/openflow-patches/controller.patch
 
     # Resume the install:
     ./boot.sh
@@ -303,7 +303,7 @@ function install_wireshark {
     # Copy coloring rules: OF is white-on-blue:
     echo "Optionally installing wireshark color filters"
     mkdir -p $HOME/.wireshark
-    cp -n $MININET_DIR/Faultynet/util/colorfilters $HOME/.wireshark
+    cp -n $MININET_DIR/faultynet/util/colorfilters $HOME/.wireshark
 
     echo "Checking Wireshark version"
     WSVER=`wireshark -v | egrep -o '[0-9\.]+' | head -1`
@@ -557,9 +557,9 @@ function nox {
 
     # Apply patches
     git checkout -b tutorial-destiny
-    git am $MININET_DIR/Faultynet/util/nox-patches/*tutorial-port-nox-destiny*.patch
+    git am $MININET_DIR/faultynet/util/nox-patches/*tutorial-port-nox-destiny*.patch
     if [ "$DIST" = "Ubuntu" ] && version_ge $RELEASE 12.04; then
-        git am $MININET_DIR/Faultynet/util/nox-patches/*nox-ubuntu12-hacks.patch
+        git am $MININET_DIR/faultynet/util/nox-patches/*nox-ubuntu12-hacks.patch
     fi
 
     # Build
